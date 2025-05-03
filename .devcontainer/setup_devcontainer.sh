84088acf4cbaa3b7b78fa1b87457f10651d34250
TDS <94196006+TdS-a0@users.noreply.github.com>
2025-05-03 23:37:48 +0900

    initial commit

---
+++ .devcontainer/setup_devcontainer.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# .devcontainer/setup_devcontainer.sh
+# これは、VS Code Dev Containerのセットアップスクリプトです。
+# このスクリプトは、.envファイルを作成し、ユーザーIDとグループIDを設定します。
+# スクリプトは、エラーが発生した場合に終了します。
+# スクリプトは、.envファイルが存在しない場合にのみ実行されます。
+# スクリプトは、.envファイルの内容を表示します
+
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+ENV_FILE="${SCRIPT_DIR}/../.env"
+
+if [ ! -f "$ENV_FILE" ]; then
+	echo ".envファイルが存在しないので作成します..."
+
+	# UIDとGIDを取得
+	USER_ID=$(id -u)
+	GROUP_ID=$(id -g)
+
+	# .envファイルに書き込み
+	{
+		echo "LOCAL_UID=${USER_ID}"
+		echo "LOCAL_GID=${GROUP_ID}"
+	} >"$ENV_FILE"
+
+	echo
+	echo ".envファイルを作成しました。内容:"
+	cat "$ENV_FILE"
+
+else
+	echo ".envファイルが存在します。内容:"
+	cat "$ENV_FILE"
+fi
+
+exit 0
